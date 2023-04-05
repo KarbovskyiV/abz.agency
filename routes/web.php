ef93d76b925a213f5a24952d8c8ed5136e4d760c
Smash2222 <karbovskyi.volodymyr@gmail.com>
2023-04-05 19:15:35 +0300

    add basic route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::redirect('/', '/home');
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/login', function () {
+    return view('auth.login');
 });
