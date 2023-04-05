eba60830304652823dff8154c3d89b007faee201
Smash2222 <karbovskyi.volodymyr@gmail.com>
2023-04-05 19:14:45 +0300

    create migrations for tables

---
+++ database/migrations/2023_04_05_095110_create_employees_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('position_id')->constrained();
+            $table->timestamps();
+
+            $table->string('name');
+            $table->string('position');
+            $table->date('date_of_employment');
+            $table->string('phone_number');
+            $table->string('email')->unique();
+            $table->decimal('salary', 10, 2);
+            $table->string('photo')->nullable();
+
+            $table->unsignedBigInteger('admin_created_id')->nullable();
+            $table->unsignedBigInteger('admin_updated_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
