3ecfbe2caf7e569e4f2820e4f1ee7e7ea338f2e8
Smash2222 <karbovskyi.volodymyr@gmail.com>
2023-04-07 12:12:23 +0300

    create index and login methods

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Employee;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $user = Employee::query()->where('email', $request->input('email'))->first();
+
+        $email = $request->input('email');
+        $password = $request->input('password');
+
+        $validator = Validator::make([
+            'email' => $email,
+            'password' => $password,
+        ], [
+            'email' => ['required', 'exists:employees', 'email'],
+            'password' => ['required', 'string']
+        ]);
+
+        if ($validator->fails()) {
+            return back()->withErrors($validator->messages());
+        }
+
+        if ($user && $user->password === $password) {
+            Auth::login($user);
+            return redirect('/');
+        }
+
+        return back()->withErrors(['email' => 'Invalid email or password']);
+    }
+}
