c9a413a8e020906e47c0e949a06dc65cb029a2ca
Smash2222 <karbovskyi.volodymyr@gmail.com>
2023-04-07 12:13:25 +0300

    add fillable and hidden and trait, also implement interface for auth

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -2,10 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use \Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Auth\Authenticatable as AuthenticatableTrait;
 
-class Employee extends Model
+class Employee extends Model implements AuthenticatableContract
 {
     use HasFactory;
+    use AuthenticatableTrait;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
 }
