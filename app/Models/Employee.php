cdd213b6efd5b39140508561a9d2f9e71f1b2382
Smash2222 <karbovskyi.volodymyr@gmail.com>
2023-04-18 20:39:20 +0300

    add mutators, relationships,

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use \Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -13,6 +14,16 @@ class Employee extends Model implements AuthenticatableContract
     use HasFactory;
     use AuthenticatableTrait;
 
+    public function position()
+    {
+        return $this->belongsTo(Position::class);
+    }
+
+    public function supervisor()
+    {
+        return $this->belongsTo(Employee::class, 'supervisor_id');
+    }
+
     protected $fillable = [
         'name',
         'email',
@@ -23,4 +34,18 @@ class Employee extends Model implements AuthenticatableContract
         'password',
         'remember_token',
     ];
+
+    protected $casts = [
+        'date_of_employment' => 'datetime:d.m.Y',
+    ];
+
+    public function getDateOfEmploymentAttribute($value)
+    {
+        return Carbon::createFromFormat('d.m.Y', $value);
+    }
+
+    public function setDateOfEmploymentAttribute($value)
+    {
+        $this->attributes['date_of_employment'] = Carbon::parse($value)->format('d.m.Y');
+    }
 }
