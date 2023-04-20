f6fe085020c1fa9c80835d8aa735a9b3f22c0064
Smash2222 <karbovskyi.volodymyr@gmail.com>
2023-04-20 11:05:03 +0300

    add error input

--- resources/views/employee/create.blade.php
+++ resources/views/employee/create.blade.php
@@ -17,13 +17,19 @@
                     <div class="text-muted">
                         File format jpg, png up to 5MB, the minimum size of 300x300px
                     </div>
+                    @if ($errors->has('photo'))
+                        <span class="text-danger">{{ $errors->first('photo') }}</span>
+                    @endif
                 </div>
 
                 <div class="form-group mb-3">
                     <label for="name" class="fw-bold mb-1">Name</label>
                     <input type="text" id="name" name="name" class="form-control" placeholder="Albert Einstein"
-                           required>
+                           >
                     <div id="nameLength" class="text-muted text-end"></div>
+                    @if ($errors->has('name'))
+                        <span class="text-danger">{{ $errors->first('name') }}</span>
+                    @endif
                 </div>
 
                 <script>
@@ -38,15 +44,21 @@
 
                 <div class="form-group mb-3">
                     <label for="phone" class="fw-bold mb-1">Phone</label>
-                    <input type="tel" id="phone" name="phone" class="form-control" placeholder="+380 (50) 458 15 47"
-                           required>
+                    <input type="tel" id="phone_number" name="phone_number" class="form-control" placeholder="+380 (50) 458 15 47"
+                           >
                     <div class="text-muted text-end">Required format +380 (xx) XXX XX XX</div>
+                    @if ($errors->has('phone_number'))
+                        <span class="text-danger">{{ $errors->first('phone_number') }}</span>
+                    @endif
                 </div>
 
                 <div class="form-group mb-3">
                     <label for="email" class="fw-bold mb-1">Email</label>
                     <input type="email" id="email" name="email" class="form-control"
-                           placeholder="alberteinstein@gmail.com" required>
+                           placeholder="alberteinstein@gmail.com" >
+                    @if ($errors->has('email'))
+                        <span class="text-danger">{{ $errors->first('email') }}</span>
+                    @endif
                 </div>
 
                 <div class="form-group mb-3">
@@ -58,23 +70,35 @@
                             @endforeach
                         </select>
                     </div>
+                    @if ($errors->has('position'))
+                        <span class="text-danger">{{ $errors->first('position') }}</span>
+                    @endif
                 </div>
 
                 <div class="form-group mb-3">
                     <label for="salary" class="fw-bold mb-1">Salary, $</label>
-                    <input type="number" id="salary" name="salary" class="form-control" placeholder="500,000" required>
+                    <input type="number" id="salary" name="salary" class="form-control" placeholder="500,000" >
+                    @if ($errors->has('salary'))
+                        <span class="text-danger">{{ $errors->first('salary') }}</span>
+                    @endif
                 </div>
 
                 <div class="form-group mb-3">
                     <label for="head" class="fw-bold mb-1">Head</label>
-                    <input type="text" class="form-control" id="head" name="head" placeholder="Frederick Banting"
-                           required>
+                    <input type="text" id="head" name="head" class="form-control"  placeholder="Frederick Banting"
+                           >
+                    @if ($errors->has('head'))
+                        <span class="text-danger">{{ $errors->first('head') }}</span>
+                    @endif
                 </div>
 
                 <div class="form-group mb-3">
                     <label for="date_of_employment" class="fw-bold mb-1">Date of Employment</label>
                     <input type="date" class="form-control" id="date_of_employment" name="date_of_employment"
-                           required>
+                           >
+                    @if ($errors->has('date_of_employment'))
+                        <span class="text-danger">{{ $errors->first('date_of_employment') }}</span>
+                    @endif
                 </div>
 
                 <div class="text-end">
