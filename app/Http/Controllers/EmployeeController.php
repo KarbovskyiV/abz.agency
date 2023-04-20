fd559b582bc73b69fff3ec3655c2de3efae754f0
Smash2222 <karbovskyi.volodymyr@gmail.com>
2023-04-20 11:04:36 +0300

    add validation, start create insert into db

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Employee;
 use App\Models\Position;
+use App\Rules\HierarchyValidationRule;
 use Illuminate\Http\Request;
 use Yajra\DataTables\Exceptions\Exception;
 
@@ -42,12 +43,39 @@ class EmployeeController extends Controller
     public function store(Request $request)
     {
         $validated = $request->validate([
+            'photo' => ['file', 'max:5000', 'mimes:jpg,png', 'dimensions:min_width=300,min_height=300'],
             'name' => ['required', 'string', 'min:2, max:256'],
-            'date_of_employment' => ['required', 'date_format:d.m.Y'],
-            'phone_number' => ['required', 'regex:^(\+38)?\s?0\d{2}\s?\d{3}\s?\d{2}\s?\d{2}$', 'min:12'],
+            'date_of_employment' => [
+                'required',
+                'date_format:Y-m-d',
+                'after_or_equal:2000-01-01',
+                'before_or_equal:today'
+            ],
+            'phone_number' => ['required', 'regex:/^(\+38)?\s?0\d{2}\s?\d{3}\s?\d{2}\s?\d{2}$/i'],
             'email' => ['required', 'email', 'unique:employees', 'max:50'],
-            'salary' => ['required', 'decimal:0,500000'],
+            'salary' => ['required', 'numeric', 'regex:/^\d+(\.\d{1,2})?$/', 'between:0,500000'],
+            'head' => [
+                'nullable',
+                new HierarchyValidationRule($request->employee),
+                'exists:employees,name',
+            ],
+            'position' => ['required', 'exists:positions,name'],
         ]);
+
+        $employees = Employee::query()->create([
+//            'position_id' => Position::query()->value('id'),
+            'supervisor_id' => Employee::query()->value('id'),
+            'name' => $validated['name'],
+            'date_of_employment' => $validated['date_of_employment'],
+            'phone_number' => $validated['phone_number'],
+            'email' => $validated['email'],
+            'password' => null,
+            'salary' => $validated['salary'],
+            'photo' => null,
+            'level' => 1,
+        ]);
+
+        dd($employees);
     }
 
     public function destroy($id)
