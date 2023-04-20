30a66ca0f8528407c1a6837528a2df9a31086d63
Smash2222 <karbovskyi.volodymyr@gmail.com>
2023-04-20 20:11:49 +0300

    add output errors

--- resources/views/employee/create.blade.php
+++ resources/views/employee/create.blade.php
@@ -10,7 +10,8 @@
                 @csrf
                 <div class="form-group mb-3">
                     <label for="photo" class="fw-bold mb-1">Photo</label>
-                    <input type="file" class="form-control-file" id="photo" name="photo" style="display: none;">
+                    <input type="file" class="form-control-file" id="photo" name="photo" value="{{ old('photo') }}"
+                           style="display: none;">
                     <div>
                         <label class="btn btn-outline-secondary w-50" for="photo">Browse</label>
                     </div>
@@ -24,8 +25,8 @@
 
                 <div class="form-group mb-3">
                     <label for="name" class="fw-bold mb-1">Name</label>
-                    <input type="text" id="name" name="name" class="form-control" placeholder="Albert Einstein"
-                           >
+                    <input type="text" id="name" name="name" class="form-control" value="{{ old('name') }}"
+                           placeholder="Albert Einstein">
                     <div id="nameLength" class="text-muted text-end"></div>
                     @if ($errors->has('name'))
                         <span class="text-danger">{{ $errors->first('name') }}</span>
@@ -44,8 +45,9 @@
 
                 <div class="form-group mb-3">
                     <label for="phone" class="fw-bold mb-1">Phone</label>
-                    <input type="tel" id="phone_number" name="phone_number" class="form-control" placeholder="+380 (50) 458 15 47"
-                           >
+                    <input type="tel" id="phone_number" name="phone_number" class="form-control"
+                           placeholder="+380 (50) 458 15 47"
+                           value="{{ old('phone_number') }}">
                     <div class="text-muted text-end">Required format +380 (xx) XXX XX XX</div>
                     @if ($errors->has('phone_number'))
                         <span class="text-danger">{{ $errors->first('phone_number') }}</span>
@@ -54,8 +56,8 @@
 
                 <div class="form-group mb-3">
                     <label for="email" class="fw-bold mb-1">Email</label>
-                    <input type="email" id="email" name="email" class="form-control"
-                           placeholder="alberteinstein@gmail.com" >
+                    <input type="email" id="email" name="email" class="form-control" value="{{ old('email') }}"
+                           placeholder="alberteinstein@gmail.com">
                     @if ($errors->has('email'))
                         <span class="text-danger">{{ $errors->first('email') }}</span>
                     @endif
@@ -66,7 +68,10 @@
                     <div>
                         <select id="position" name="position">
                             @foreach($positions as $position)
-                                <option value="{{ $position->name }}">{{ $position->name }}</option>
+                                <option
+                                    value="{{ $position->name }}" {{ old('position') === $position->name ? 'selected' : '' }}>
+                                    {{ $position->name }}
+                                </option>
                             @endforeach
                         </select>
                     </div>
@@ -77,7 +82,8 @@
 
                 <div class="form-group mb-3">
                     <label for="salary" class="fw-bold mb-1">Salary, $</label>
-                    <input type="number" id="salary" name="salary" class="form-control" placeholder="500,000" >
+                    <input type="number" id="salary" name="salary" class="form-control" value="{{ old('salary') }}"
+                           placeholder="500,000">
                     @if ($errors->has('salary'))
                         <span class="text-danger">{{ $errors->first('salary') }}</span>
                     @endif
@@ -85,8 +91,8 @@
 
                 <div class="form-group mb-3">
                     <label for="head" class="fw-bold mb-1">Head</label>
-                    <input type="text" id="head" name="head" class="form-control"  placeholder="Frederick Banting"
-                           >
+                    <input type="text" id="head" name="head" class="form-control" value="{{ old('head') }}"
+                           placeholder="Frederick Banting">
                     @if ($errors->has('head'))
                         <span class="text-danger">{{ $errors->first('head') }}</span>
                     @endif
@@ -94,8 +100,7 @@
 
                 <div class="form-group mb-3">
                     <label for="date_of_employment" class="fw-bold mb-1">Date of Employment</label>
-                    <input type="date" class="form-control" id="date_of_employment" name="date_of_employment"
-                           >
+                    <input type="date" class="form-control" id="date_of_employment" name="date_of_employment">
                     @if ($errors->has('date_of_employment'))
                         <span class="text-danger">{{ $errors->first('date_of_employment') }}</span>
                     @endif
